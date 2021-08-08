fbe0f85d4f9da67c11f411f965999c819f41a972
Nedim <mr.ramicevic@gmail.com>
2021-08-08 09:05:46 +0300

    addCard

---
+++ src/Card.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+const Card = () => {
+  return (
+    <div>
+      <div className="card">
+        <img
+          src="https://www.w3schools.com/howto/img_avatar.png"
+          alt="Avatar"
+          style={{ width: "100%" }}
+        ></img>
+        <div className="container">
+          <h4>
+            <b>John Doe</b>
+          </h4>
+          <p>Architect & Engineer</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
