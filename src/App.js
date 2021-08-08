ac8cec95b44f52968313220eeb08ac4727fb06d5
Nedim <mr.ramicevic@gmail.com>
2021-08-08 11:28:17 +0300

    conditionalrendering

--- src/App.js
+++ src/App.js
@@ -5,28 +5,37 @@ import faker from "faker";
 
 function App() {
   const [name, setName] = useState("naber lan");
+  const [showCard, setShowCard] = useState(true);
+  const showCardFun = () => {
+    setShowCard(!showCard);
+    console.log("nabers", showCard);
+  };
+  const chaneInput = (name) => setName(name.target.value);
+  const setNewName = () =>
+    setName(`${faker.name.firstName()} ${faker.name.lastName()}`);
   const btns = (
     <div>
       <button className="btnYes" onClick={setNewName}>
-        Yes
+        Change Name
       </button>
-      <button className="btnNo">No</button>
     </div>
   );
-  function setNewName() {
-    setName(`${faker.name.firstName()} ${faker.name.lastName()}`);
-  }
-  const chaneInput = (name) => setName(name.target.value);
+
   return (
     <div className="App">
-      <Card
-        name={name}
-        title={faker.name.jobTitle()}
-        img={faker.image.avatar()}
-        onChangeInput={chaneInput}
-      >
-        {btns}
-      </Card>
+      <button className="btnNo" onClick={showCardFun}>
+        Show Card
+      </button>
+      {showCard ? (
+        <Card
+          name={name}
+          title={faker.name.jobTitle()}
+          img={faker.image.avatar()}
+          onChangeInput={chaneInput}
+        >
+          {btns}
+        </Card>
+      ) : null}
       <Card
         name={`${faker.name.firstName()} ${faker.name.lastName()}`}
         title={faker.name.jobTitle()}
