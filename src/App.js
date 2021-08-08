61876810b97efd5df6bcb2324329c60876c87591
Nedim <mr.ramicevic@gmail.com>
2021-08-08 11:01:24 +0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import "./App.css";
+import React, { useState } from "react";
 import Card from "./Card";
 import faker from "faker";
 
 function App() {
+  const [name, setName] = useState("naber lan");
   const btns = (
     <div>
-      <button className="btnYes">Yes</button>
+      <button className="btnYes" onClick={setNewName}>
+        Yes
+      </button>
       <button className="btnNo">No</button>
     </div>
   );
+  function setNewName() {
+    setName(`${faker.name.firstName()} ${faker.name.lastName()}`);
+  }
   return (
     <div className="App">
       <Card
-        name={`${faker.name.firstName()} ${faker.name.lastName()}`}
+        name={name}
         title={faker.name.jobTitle()}
         img={faker.image.avatar()}
       >
@@ -24,27 +31,6 @@ function App() {
         img={faker.image.avatar()}
       >
         {btns}
-      </Card>
-      <Card
-        name={`${faker.name.firstName()} ${faker.name.lastName()}`}
-        title={faker.name.jobTitle()}
-        img={faker.image.avatar()}
-      >
-        btns
-      </Card>
-      <Card
-        name={`${faker.name.firstName()} ${faker.name.lastName()}`}
-        title={faker.name.jobTitle()}
-        img={faker.image.avatar()}
-      >
-        btns
-      </Card>
-      <Card
-        name={`${faker.name.firstName()} ${faker.name.lastName()}`}
-        title={faker.name.jobTitle()}
-        img={faker.image.avatar()}
-      >
-        btns
       </Card>
     </div>
   );
