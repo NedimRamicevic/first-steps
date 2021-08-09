42e28303d1571b882438587f65b248e11c727e2d
Nedim <mr.ramicevic@gmail.com>
2021-08-09 10:55:27 +0300

    conditioninrender

--- src/denemes.js
+++ src/denemes.js
@@ -11,34 +11,46 @@ function Denemes() {
 
   //   return <div className="App">{peopleLis}</div>;
 
-  const friends = [
-    {
-      title: "Yummmmmmm",
-      src: "https://content.codecademy.com/courses/React/react_photo-monkeyweirdo.jpg",
-    },
-    {
-      title: "Hey Guys!  Wait Up!",
-      src: "https://content.codecademy.com/courses/React/react_photo-earnestfrog.jpg",
-    },
-    {
-      title: "Yikes",
-      src: "https://content.codecademy.com/courses/React/react_photo-alpaca.jpg",
-    },
-  ];
+  //   const friends = [
+  //     {
+  //       title: "Yummmmmmm",
+  //       src: "https://content.codecademy.com/courses/React/react_photo-monkeyweirdo.jpg",
+  //     },
+  //     {
+  //       title: "Hey Guys!  Wait Up!",
+  //       src: "https://content.codecademy.com/courses/React/react_photo-earnestfrog.jpg",
+  //     },
+  //     {
+  //       title: "Yikes",
+  //       src: "https://content.codecademy.com/courses/React/react_photo-alpaca.jpg",
+  //     },
+  //   ];
+
+  //   // New component class starts here:
+  //   class Friend extends React.Component {
+  //     render() {
+  //       var friend = friends[0];
+  //       return (
+  //         <div>
+  //           <h1>{friend.title}</h1>
+  //           <img src={friend.src} />
+  //         </div>
+  //       );
+  //     }
+  //   }
+
+  const fiftyFifty = Math.random() < 0.5;
 
   // New component class starts here:
-  class Friend extends React.Component {
+  class TonightsPlan extends React.Component {
     render() {
-      var friend = friends[0];
-      return (
-        <div>
-          <h1>{friend.title}</h1>
-          <img src={friend.src} />
-        </div>
+      return fiftyFifty ? (
+        <h1>Tonight I'm going out WOOO</h1>
+      ) : (
+        <h1>Tonight I'm going to bed WOOO</h1>
       );
     }
   }
-
-  return <Friend />;
+  return <TonightsPlan />;
 }
 export default Denemes;
