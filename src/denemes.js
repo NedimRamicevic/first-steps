065c335fe0b85956853a8afe755816c9387676f5
Nedim <mr.ramicevic@gmail.com>
2021-08-10 09:30:42 +0300

    talkcomp

--- src/denemes.js
+++ src/denemes.js
@@ -41,50 +41,63 @@ import React from "react";
 //   const fiftyFifty = Math.random() < 0.5;
 
 //   // New component class starts here:
-export class TonightsPlan extends React.Component {
-  scream() {
+export function TonightsPlan() {
+  const scream = () => {
     alert("nabers");
-  }
+  };
 
-  render() {
-    const fiftyFifty = Math.random() < 0.5;
-    return fiftyFifty ? (
-      <div>
-        <h1>Tonight I'm going out WOOO</h1>
-        <button onClick={this.scream}>Nabers</button>
-      </div>
-    ) : (
-      <div>
-        <h1>Tonight I'm going to bed WOOO</h1>
-        <button onClick={this.scream}>Nabers</button>
-      </div>
-    );
+  const fiftyFifty = Math.random() < 0.5;
+  return fiftyFifty ? (
+    <div>
+      <h1>Tonight I'm going out WOOO</h1>
+      <button onClick={scream}>Nabers</button>
+    </div>
+  ) : (
+    <div>
+      <h1>Tonight I'm going to bed WOOO</h1>
+      <button onClick={scream}>Nabers</button>
+    </div>
+  );
+}
+
+// New component class starts here:
+
+export function Welcome(props) {
+  if (props.name === "Wolfgang Amadeus Mozart") {
+    return <h2>hello sir it is truly great to meet you here on the web</h2>;
+  } else {
+    return <h2>WELCOME "2" MY WEB SITE BABYYY!!!!!</h2>;
   }
 }
 
 // New component class starts here:
 
-export class Welcome extends React.Component {
-  render() {
-    if (this.props.name === "Wolfgang Amadeus Mozart") {
-      return <h2>hello sir it is truly great to meet you here on the web</h2>;
-    } else {
-      return <h2>WELCOME "2" MY WEB SITE BABYYY!!!!!</h2>;
-    }
+export function Greeting(props) {
+  if (props.signedIn === false) {
+    return <h1>GO AWAY</h1>;
+  } else {
+    return <h1>Hi there, {props.name}!</h1>;
   }
 }
 
 // New component class starts here:
 
-export class Greeting extends React.Component {
-  render() {
-    if (this.props.signedIn === false) {
-      return <h1>GO AWAY</h1>;
-    } else {
-      return <h1>Hi there, {this.props.name}!</h1>;
+export function Talker(props) {
+  const talk = () => {
+    let speech = "";
+    for (let i = 0; i < 10000; i++) {
+      speech += "blah ";
     }
-  }
+    alert(speech);
+  };
+
+  return <Button talk={talk} />;
 }
+
+export function Button(props) {
+  return <button>Click me!</button>;
+}
+
 //   return <TonightsPlan />;
 // }
 // export default Denemes;
