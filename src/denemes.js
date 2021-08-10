b7c2f623e53c2cb2f88847f98d2b2fb4251b2129
Nedim <mr.ramicevic@gmail.com>
2021-08-10 09:09:30 +0300

    anotherComponent

--- src/denemes.js
+++ src/denemes.js
@@ -1,65 +1,90 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
-export function Denemes() {
-  //   const people = ["Rowe", "Prevost", "Gare"];
+// export function Denemes() {
+//   //   const people = ["Rowe", "Prevost", "Gare"];
 
-  //   const peopleLis = people.map(
-  //     (person, i) => (i === 1 ? <li key={"person_" + i}>{person}</li> : "naber    ")
-  //     // expression goes here:
-  //   );
+//   //   const peopleLis = people.map(
+//   //     (person, i) => (i === 1 ? <li key={"person_" + i}>{person}</li> : "naber    ")
+//   //     // expression goes here:
+//   //   );
 
-  //   return <div className="App">{peopleLis}</div>;
+//   //   return <div className="App">{peopleLis}</div>;
 
-  //   const friends = [
-  //     {
-  //       title: "Yummmmmmm",
-  //       src: "https://content.codecademy.com/courses/React/react_photo-monkeyweirdo.jpg",
-  //     },
-  //     {
-  //       title: "Hey Guys!  Wait Up!",
-  //       src: "https://content.codecademy.com/courses/React/react_photo-earnestfrog.jpg",
-  //     },
-  //     {
-  //       title: "Yikes",
-  //       src: "https://content.codecademy.com/courses/React/react_photo-alpaca.jpg",
-  //     },
-  //   ];
+//   //   const friends = [
+//   //     {
+//   //       title: "Yummmmmmm",
+//   //       src: "https://content.codecademy.com/courses/React/react_photo-monkeyweirdo.jpg",
+//   //     },
+//   //     {
+//   //       title: "Hey Guys!  Wait Up!",
+//   //       src: "https://content.codecademy.com/courses/React/react_photo-earnestfrog.jpg",
+//   //     },
+//   //     {
+//   //       title: "Yikes",
+//   //       src: "https://content.codecademy.com/courses/React/react_photo-alpaca.jpg",
+//   //     },
+//   //   ];
 
-  //   // New component class starts here:
-  //   class Friend extends React.Component {
-  //     render() {
-  //       var friend = friends[0];
-  //       return (
-  //         <div>
-  //           <h1>{friend.title}</h1>
-  //           <img src={friend.src} />
-  //         </div>
-  //       );
-  //     }
-  //   }
+//   //   // New component class starts here:
+//   //   class Friend extends React.Component {
+//   //     render() {
+//   //       var friend = friends[0];
+//   //       return (
+//   //         <div>
+//   //           <h1>{friend.title}</h1>
+//   //           <img src={friend.src} />
+//   //         </div>
+//   //       );
+//   //     }
+//   //   }
 
-  const fiftyFifty = Math.random() < 0.5;
+//   const fiftyFifty = Math.random() < 0.5;
 
-  // New component class starts here:
-  class TonightsPlan extends React.Component {
-    scream() {
-      alert("nabers");
-    }
-    render() {
-      return fiftyFifty ? (
-        <div>
-          <h1>Tonight I'm going out WOOO</h1>
-          <button onClick={this.scream}>Nabers</button>
-        </div>
-      ) : (
-        <div>
-          <h1>Tonight I'm going to bed WOOO</h1>
-          <button onClick={this.scream}>Nabers</button>
-        </div>
-      );
+//   // New component class starts here:
+export class TonightsPlan extends React.Component {
+  scream() {
+    alert("nabers");
+  }
+
+  render() {
+    const fiftyFifty = Math.random() < 0.5;
+    return fiftyFifty ? (
+      <div>
+        <h1>Tonight I'm going out WOOO</h1>
+        <button onClick={this.scream}>Nabers</button>
+      </div>
+    ) : (
+      <div>
+        <h1>Tonight I'm going to bed WOOO</h1>
+        <button onClick={this.scream}>Nabers</button>
+      </div>
+    );
+  }
+}
+
+// New component class starts here:
+
+export class Welcome extends React.Component {
+  render() {
+    if (this.props.name === "Wolfgang Amadeus Mozart") {
+      return <h2>hello sir it is truly great to meet you here on the web</h2>;
+    } else {
+      return <h2>WELCOME "2" MY WEB SITE BABYYY!!!!!</h2>;
     }
   }
-  return <TonightsPlan />;
 }
+
+// New component class starts here:
+
+export class Greeting extends React.Component {
+  render() {
+    if (this.props.signedIn === false) {
+      return <h1>GO AWAY</h1>;
+    } else {
+      return <h1>Hi there, {this.props.name}!</h1>;
+    }
+  }
+}
+//   return <TonightsPlan />;
+// }
 // export default Denemes;
