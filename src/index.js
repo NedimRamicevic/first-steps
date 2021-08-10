bbd00d781f4e28972bc1188d5727abbda5bdc0aa
Nedim <mr.ramicevic@gmail.com>
2021-08-10 08:38:51 +0300

    toApp

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ import App from "./App";
 import { Denemes } from "./denemes";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Denemes />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
 // ReactDOM.render(
 //   <React.StrictMode>
-//     <App />
+//     <Denemes />
 //   </React.StrictMode>,
 //   document.getElementById("root")
 // );
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
